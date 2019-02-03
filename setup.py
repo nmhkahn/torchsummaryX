558b0ec4e5f8efdbdf4244cc7b5e10ce66095910
Namhyuk Ahn <nmhkahn@gmail.com>
2019-02-03 16:38:49 +0900

    Update to v1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name         = "torchsummaryX",
-    version      = "1.0.0",
+    version      = "1.1.0",
     description  = "Improved visualization tool of torchsummary.",
     author       = "Namhyuk Ahn",
     author_email = "nmhkahn@gmail.com",
