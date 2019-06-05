36b54e837ac2eac54645c8abd1f2c849945eac1f
Namhyuk Ahn <nmhkahn@gmail.com>
2019-06-05 19:02:56 +0900

    Fix bugs

--- torchsummaryX/torchsummaryX.py
+++ torchsummaryX/torchsummaryX.py
@@ -17,11 +17,11 @@ def summary(model, x, *args, **kwargs):
         def hook(module, inputs, outputs):
             cls_name = str(module.__class__).split(".")[-1].split("'")[0]
             module_idx = len(summary)
-            
+
             # Lookup name in a dict that includes parents
             for name, item in module_names.items():
                 if item == module:
-                    key = '{}_{}'.format(module_idx, name)
+                    key = "{}_{}".format(module_idx, name)
 
             info = OrderedDict()
             info["id"] = id(module)
@@ -69,55 +69,59 @@ def summary(model, x, *args, **kwargs):
         # ignore Sequential and ModuleList
         if not module._modules:
             hooks.append(module.register_forward_hook(hook))
-            
+
     module_names = get_names_dict(model)
 
     hooks = []
     summary = OrderedDict()
 
     model.apply(register_hook)
-    
+
     with torch.no_grad():
         model(x) if not (kwargs or args) else model(x, *args, **kwargs)
 
     for hook in hooks:
         hook.remove()
-    
+
     # Use pandas to align the columns
     df = pd.DataFrame(summary).T
-    df['Mult-Adds (M)'] = pd.to_numeric(df['macs'], errors='coerce')/1e6
-    df['Params (K)'] = pd.to_numeric(df['params'], errors='coerce')/1e3
+    df["Mult-Adds (M)"] = pd.to_numeric(df["macs"], errors="coerce")/1e6
+    df["Params (K)"] = pd.to_numeric(df["params"], errors="coerce")/1e3
     df = df.rename(columns=dict(
-        ksize='Kernel Shape',
-        out='Output Shape',
+        ksize="Kernel Shape",
+        out="Output Shape",
     ))
-    df.index.name = 'Layer'
-    df = df[['Kernel Shape',  'Output Shape', 'Params (K)',   'Mult-Adds (M)']]
-    
-    print("="*100)
-    print(df.replace(np.nan, '-')
-    print("="*100)
-    print(df.sum())
-    print("="*100)
+    df.index.name = "Layer"
+    df = df[["Kernel Shape", "Output Shape", "Params (K)", "Mult-Adds (M)"]]
+    df_sum = df.sum()
+
+    max_repr_width = max([len(row) for row in df.to_string().split("\n")])
+
+    print("="*max_repr_width)
+    print(df.replace(np.nan, "-"))
+    print("-"*max_repr_width)
+    print("Params (K): ", df_sum["Params (K)"])
+    print("Mult-Adds (M): ", df_sum["Mult-Adds (M)"])
+    print("="*max_repr_width)
 
     return df
-    
+
 def get_names_dict(model):
     """Recursive walk to get names including path."""
     names = {}
 
-    def _get_names(module, parent_name=''):
-        for key, module in module.named_children():
-            cls_name = str(module.__class__).split(".")[-1].split("'")[0]
-            num_named_children = len(list(module.named_children()))
-            if num_named_children>0:
-                name = parent_name + '.' + key if parent_name else key  
+    def _get_names(module, parent_name=""):
+        for key, m in module.named_children():
+            cls_name = str(m.__class__).split(".")[-1].split("'")[0]
+            num_named_children = len(list(m.named_children()))
+            if num_named_children > 0:
+                name = parent_name + "." + key if parent_name else key
             else:
-                name = parent_name + '.' + cls_name + '_'+ key if parent_name else key
-            names[name] = module
-            
-            if isinstance(module, torch.nn.Module):
-                _get_names(module, parent_name=name)  
-            
+                name = parent_name + "." + cls_name + "_"+ key if parent_name else key
+            names[name] = m
+
+            if isinstance(m, torch.nn.Module):
+                _get_names(m, parent_name=name)
+
     _get_names(model)
     return names
