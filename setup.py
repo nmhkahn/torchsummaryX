d61b3d703263ec86cda6d3c07e8e08cacbf024a0
Namhyuk Ahn <nmhkahn@gmail.com>
2019-06-05 19:17:05 +0900

    Update to v1.2.0

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name         = "torchsummaryX",
-    version      = "1.1.0",
-    description  = "Improved visualization tool of torchsummary.",
-    author       = "Namhyuk Ahn",
-    author_email = "nmhkahn@gmail.com",
-    url          = "https://github.com/nmhkahn/torchsummaryX",
-    packages     =["torchsummaryX"],
+    name             = "torchsummaryX",
+    version          = "1.2.0",
+    description      = "Improved visualization tool of torchsummary.",
+    author           = "Namhyuk Ahn",
+    author_email     = "nmhkahn@gmail.com",
+    url              = "https://github.com/nmhkahn/torchsummaryX",
+    packages         =["torchsummaryX"],
+    install_requires = ["torch", "numpy", "pandas"],
 )
